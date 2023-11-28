9376364a964f6c90112a7c3a7eb0891c46a9fb1b
shaikhfarheen11 <farheens554>
2023-11-28 16:39:46 +0530

    first commit

--- redux-demo.js
+++ redux-demo.js
@@ -1,20 +1,32 @@
 const redux = require('redux');
-const counterReducer = (state = { counter : 0}, action) => {
-    if (action.type === 'increment'){
-    return {
-        counter: state.counter + 1
-    };
-}
-return state;
-}
-const store = redux.createStore(counterReducer);
 
+const counterReducer = (state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {
+                counter: state.counter + 1
+            };
+        case 'decrement':
+            return {
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
+};
+
+const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
     const latestState = store.getState();
     console.log(latestState);
 };
+
 store.subscribe(counterSubscriber);
+
+
 for (let i = 0; i < 5; i++) {
     store.dispatch({ type: 'increment' });
 }
+
+store.dispatch({ type: 'decrement' });
